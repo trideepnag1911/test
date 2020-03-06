ade977e518311b94a7cafb70387be0393d397a61
Debian <admin@ip-172-31-17-81.ap-south-1.compute.internal>
2020-03-06 08:37:30 +0000

    modified script

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-Author=(`cat ../Books/Books.xml | grep author | sed 's/<author>//g' | sed 's/<\/author>//g' | sed 's/,//g'| sed 's/^ *//g'| sed 's/ /_/g'`)
-Book_ID=(`cat ../Books/Books.xml | grep "book id" | sed 's/<book id="//g'| sed 's/">//g'`)
-Line_Count=`cat ../Books/Books.xml | grep author | wc -l`
+Author=(`cat Books.xml | grep author | sed 's/<author>//g' | sed 's/<\/author>//g' | sed 's/,//g'| sed 's/^ *//g'| sed 's/ /_/g'`)
+Book_ID=(`cat Books.xml | grep "book id" | sed 's/<book id="//g'| sed 's/">//g'`)
+Line_Count=`cat Books.xml | grep author | wc -l`
 Count=0
  for i in $(seq 1 1 $Line_Count)
  do
